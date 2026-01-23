5a7f522ec9242dacbef406cf319f9199939169ed
Saif Al-Din Ali <saif-canada@hotmail.com>
2026-01-23 08:53:44 -0800

    Add fake defaults to the example files (#24)
    * Update variable
    
    * Create migrate example
    
    * Add fake defaults to the example files
    
    ---------
    
    Co-authored-by: Saif Al-Din Ali <saifaldinali@microsoft.com>

--- examples/jobs/variables.tf
+++ examples/jobs/variables.tf
@@ -11,25 +11,25 @@ variable "instance_type" {
 
 variable "project_name" {
   type        = string
-  default     = "saif-project-120126"
+  default     = "my-migrate-project"
   description = "The name of the Azure Migrate project"
 }
 
 variable "replication_vault_id" {
   type        = string
-  default     = "/subscriptions/f6f66a94-f184-45da-ac12-ffbfd8a6eb29/resourceGroups/saif-project-120126-rg/providers/Microsoft.DataReplication/replicationVaults/saif-project-16712replicationvault"
+  default     = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/my-migrate-project-rg/providers/Microsoft.DataReplication/replicationVaults/myprojectreplicationvault"
   description = "The full resource ID of the replication vault (optional, derived from project if not provided)"
 }
 
 variable "resource_group_name" {
   type        = string
-  default     = "saif-project-120126-rg"
+  default     = "my-migrate-project-rg"
   description = "The name of the resource group containing the Azure Migrate project"
 }
 
 variable "subscription_id" {
   type        = string
-  default     = "f6f66a94-f184-45da-ac12-ffbfd8a6eb29"
+  default     = "00000000-0000-0000-0000-000000000000"
   description = "The Azure subscription ID where resources will be deployed"
 }
 
--- examples/list/variables.tf
+++ examples/list/variables.tf
@@ -17,19 +17,19 @@ variable "location" {
 
 variable "project_name" {
   type        = string
-  default     = "saif-project-120126"
+  default     = "my-migrate-project"
   description = "The name of the Azure Migrate project (used to auto-discover vault)"
 }
 
 variable "resource_group_name" {
   type        = string
-  default     = "saif-project-120126-rg"
+  default     = "my-migrate-project-rg"
   description = "The name of the resource group containing the Azure Migrate project"
 }
 
 variable "subscription_id" {
   type        = string
-  default     = "f6f66a94-f184-45da-ac12-ffbfd8a6eb29"
+  default     = "00000000-0000-0000-0000-000000000000"
   description = "The Azure subscription ID where resources will be deployed"
 }
 
